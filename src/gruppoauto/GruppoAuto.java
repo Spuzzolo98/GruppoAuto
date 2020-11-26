42f2084aa86598ba569c1d7f4fd4d275945ebd19
Lorenzo <60008393+Spuzzolo98@users.noreply.github.com>
2020-11-26 13:56:35 +0100

    Update GruppoAuto.java

--- src/gruppoauto/GruppoAuto.java
+++ src/gruppoauto/GruppoAuto.java
@@ -1,8 +1,19 @@
 package gruppoauto;
+import classi.Automobile;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GruppoAuto {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        
+        
+        
+        
+        
+        
         
     }
     
